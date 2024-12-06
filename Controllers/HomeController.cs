1158f48a214e9f485ef20356632ea65246c98d6d
Randy Hardianto <randy.hardianto@flexidev.co>
2024-12-06 08:15:25 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SL_AIPrompt.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!string.IsNullOrEmpty(context.HttpContext.Request.Query["culture"]))
+            {
+                CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(context.HttpContext.Request.Query["culture"]);
+            }
+            base.OnActionExecuting(context);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
